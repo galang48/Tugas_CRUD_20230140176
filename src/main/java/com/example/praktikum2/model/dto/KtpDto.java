92fb3f286e8e0b8495275624bad1edb48c42495b
galang48 <galangyudha1945@gmail.com>
2026-03-12 11:12:58 +0700

    Menambahkan DTO KtpDto untuk representasi data KTP pada response API

---
+++ src/main/java/com/example/praktikum2/model/dto/KtpDto.java
@@ -0,0 +1,61 @@
+package com.example.praktikum2.model.dto;
+
+import java.util.Date;
+
+public class KtpDto {
+
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private Date tanggalLahir;
+    private String jenisKelamin;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomorKtp() {
+        return nomorKtp;
+    }
+
+    public void setNomorKtp(String nomorKtp) {
+        this.nomorKtp = nomorKtp;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+}
